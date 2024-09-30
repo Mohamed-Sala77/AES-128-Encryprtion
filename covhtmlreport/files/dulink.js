a59f395530834415b9408231cd5d000626470a25
Mohamed-Sala77 <105118807+Mohamed-Sala77@users.noreply.github.com>
2024-09-30 14:49:56 +0300

    delete unimportant files

--- covhtmlreport/files/dulink.js
+++
@@ -1,2 +0,0 @@
-var g_data = {"8":["work.AES_Encrypt",100.00,1],"6":["work.addRoundKey",100.00,1],"2":["work.encryptRound",85.71,1],"9":["work.intf1",100.00,1],"7":["work.keyExpansion",76.06,1],"1":["work.mixColumns",100.00,1],"10":["work.my_top",28.24,1],"3":["work.sbox",99.87,1],"4":["work.shiftRows",100.00,1],"5":["work.subBytes",100.00,1]};
-processDuLinks(g_data);
